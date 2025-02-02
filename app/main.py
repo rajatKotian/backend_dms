d6ceb05bde056fec00855bd87c7e309f4fd90fe7
rajatKotian <rajatkotian@yahoo.com>
2025-02-02 14:59:57 +0530

    FEATURE: Health check updated

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+@app.get("/health")
+def health():
+    return {"status":True,"message": "Systemn is up and running"}
